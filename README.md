3e40ddeec8607161fb8d263653e8a08064077a65
Diego Colín <luiscolin764@gmail.com>
2025-05-28 20:54:40 -0600

    Added commands to get the backend running to the readme. Got the backed up and running succesfully, solved baseModel import

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # AI Assistant Backend
+
+
+
+Activate virtual environment: source venv/Scripts/activate
+Launch Backend on http://127.0.0.1:8000: uvicorn main:app --reload
+
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+# Define the expected structure of the incoming JSON
+class QuestionRequest(BaseModel):
+    question: str
+
+@app.post("/ask")
+async def ask_ai(request: QuestionRequest):
+    return {"received": request.question}
 
 @app.get("/")
 def read_root():
